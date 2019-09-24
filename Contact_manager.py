fce3a1654e262072714df4ecb44c3f860f22f62f
robert <kaczurr@gmail.com>
2019-09-24 03:22:17 -0400

    Added file auto loading.

--- Contact_manager.py
+++ Contact_manager.py
@@ -2,15 +2,18 @@
 import json
 
 
+
 class Contact_manager:
+
     def __init__(self):
         self.contact_book = {}
+        self.file_name = "contacts.txt"
 
     def create_contact(self):
         contact_ID = input("Enter contact ID or code or nickname\n")
-        self.contact_book[contact_ID] = {"contact_ID":contact_ID, "contact_name": "", "contact_billing": "", "contact_LTD": "", "inovice_count": ""}
+        self.contact_book[contact_ID] = {"contact_ID": contact_ID, "contact_name": "", "contact_billing": "", "contact_LTD": "", "inovice_count": ""}
         user_input = input("enter contact name\n")
-        self.contact_book[contact_ID]["contact_name"] = user_input        
+        self.contact_book[contact_ID]["contact_name"] = user_input
         user_input = input("enter contact billing information\n")
         self.contact_book[contact_ID]["contact_billing"] = user_input
         user_input = input("enter contact ltd number\n")
@@ -22,17 +25,17 @@ class Contact_manager:
         print("2:\t Change contact billing address")
         print("3:\t Change contact ltd number")
         user_input = input("Enter selection:\n")
-        
+
         if(int(user_input) == 1):
             print("Current contact name is: {}".format(self.contact_book[contact_ID]["contact_name"]))
             user_input2 = input("Enter new value\n")
             self.contact_book[contact_ID]["contact_name"] = user_input2
-            
+
         if(int(user_input) == 2):
             print("Current contact billing address is: {}".format(self.contact_book[contact_ID]["contact_billing"]))
             user_input2 = input("Enter new value\n")
             self.contact_book[contact_ID]["contact_billing"] = user_input2
-            
+
         if(int(user_input) == 3):
             print("Current company ltd is: {}".format(self.contact_book[contact_ID]["contact_LTD"]))
             user_input2 = input("Enter new value\n")
@@ -45,24 +48,28 @@ class Contact_manager:
         print("2:\tDisplay Contact billing addresses")
         print("3:\tDiplay Contacts and contact ltd")
         user_input = input("Enter Selection\n")
+        
         if(int(user_input) == 1):
             for k in self.contact_book.values():
-                print("Contact ID: {}\nContact Name: {}".format(k['contact_ID'],k['contact_name']))
+                print("_______________")
+                print("Contact ID: {}\nContact Name: {}".format(k['contact_ID'], k['contact_name']))
             input("....")
         if(int(user_input) == 2):
             for k in self.contact_book.values():
-                print("Contact ID: {}\nContact Name: {}".format(k['contact_ID'],k['contact_billing']))
+                print("_______________")
+                print("Contact ID: {}\nContact Name: {}".format(k['contact_ID'], k['contact_billing']))
             input("....")
         if(int(user_input) == 3):
             for k in self.contact_book.values():
-                print("Contact ID: {}\nContact Name: {}".format(k['contact_ID'],k['contact_LTD']))
+                print("_______________")
+                print("Contact ID: {}\nContact Name: {}".format(k['contact_ID'], k['contact_LTD']))
             input("....")
-                   
+
 
     def save_contacts(self):
-        with open("contacts.txt", "w") as file:
+        with open(self.file_name, "w") as file:
             file.write(json.dumps(self.contact_book))
 
     def load_contacts(self):
-        with open("contacts.txt", "r+") as file:
+        with open(self.file_name, "r+") as file:
             self.contact_book = json.load(file)
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from invoice_manager import Invoice_manager
 from Contact_manager import Contact_manager
+import os.path
+import os
 
 
 inv_mgr = Invoice_manager()
 con_mgr = Contact_manager()
 # Menu
 
+def auto_load():
+    if(os.path.isfile(con_mgr.file_name)) == True:
+        con_mgr.load_contacts()
+        return True
+    else:
+        open(con_mgr.file_name, 'a').close()
+
 
 def main_menu():
+    os.system('cls||clear')
+    if(auto_load() == True):
+        print("Contact File Auto Loaded")
+    
     print("options should be:")
     print("1:\tCreate contact")
     print("2:\tView contacts")
@@ -15,29 +28,28 @@ def main_menu():
     print("4:\tload contacts")
     print("5:\tmodify contact")
     input_option = input("Enter Option:\n")
-    
-    
+
     if(int(input_option) == 1):
         con_mgr.create_contact()
         print(con_mgr.contact_book)
         main_menu()
-        
+
     if(int(input_option) == 2):
         print("launched 2")
         con_mgr.display_contact()
-    
+
     if(int(input_option) == 3):
         con_mgr.save_contacts()
-    
+
     if(int(input_option) == 4):
         con_mgr.load_contacts()
         main_menu()
-        
+
     if(int(input_option) == 5):
         user_input = input("Enter Contact ID\n")
         con_mgr.modify_contact(user_input)
         main_menu()
-    
+
     else:
         print("invalid selection")
         main_menu()
