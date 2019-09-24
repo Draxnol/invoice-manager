27d91dbaefee9dfd4f471170562e75f42ab8f06d
robert <kaczurr@gmail.com>
2019-09-24 02:45:05 -0400

    Added more display options

--- Contact_manager.py
+++ Contact_manager.py
@@ -1,49 +1,68 @@
-#Contact Name, Contact Billing address, Contact ltd, Contact invoice count
+# Contact Name, Contact Billing address, Contact ltd, Contact invoice count
 import json
+
+
 class Contact_manager:
     def __init__(self):
         self.contact_book = {}
+
     def create_contact(self):
-        contact_ID = input("Enter contact ID or code or nickname")
-        self.contact_book[contact_ID] = {"contact_name":"", "contact_billing":"", "contact_LTD":"", "inovice_count":""} 
+        contact_ID = input("Enter contact ID or code or nickname\n")
+        self.contact_book[contact_ID] = {"contact_ID":contact_ID, "contact_name": "", "contact_billing": "", "contact_LTD": "", "inovice_count": ""}
         user_input = input("enter contact name\n")
-        self.contact_book[contact_ID]["contact_name"] = user_input
-        user_input = input("enter contact billing information")
+        self.contact_book[contact_ID]["contact_name"] = user_input        
+        user_input = input("enter contact billing information\n")
         self.contact_book[contact_ID]["contact_billing"] = user_input
-        user_input = input("enter contact ltd number")
+        user_input = input("enter contact ltd number\n")
         self.contact_book[contact_ID]["contact_LTD"] = user_input
+
     def modify_contact(self, contact_ID):
         print("Options should be:")
         print("1:\t Change contact name")
         print("2:\t Change contact billing address")
         print("3:\t Change contact ltd number")
-        user_input = input("Enter selection:")
+        user_input = input("Enter selection:\n")
+        
         if(int(user_input) == 1):
             print("Current contact name is: {}".format(self.contact_book[contact_ID]["contact_name"]))
             user_input2 = input("Enter new value\n")
             self.contact_book[contact_ID]["contact_name"] = user_input2
+            
         if(int(user_input) == 2):
             print("Current contact billing address is: {}".format(self.contact_book[contact_ID]["contact_billing"]))
             user_input2 = input("Enter new value\n")
             self.contact_book[contact_ID]["contact_billing"] = user_input2
+            
         if(int(user_input) == 3):
             print("Current company ltd is: {}".format(self.contact_book[contact_ID]["contact_LTD"]))
             user_input2 = input("Enter new value\n")
             self.contact_book[contact_ID]["contact_LTD"] = user_input2
+
     def display_contact(self):
-       #Here are display options
+       # Here are display options
         print("Display options should be")
-        print("1:\tDisplay Contacts and contact name")
+        print("1:\tDisplay Contact and contact name")
         print("2:\tDisplay Contact billing addresses")
         print("3:\tDiplay Contacts and contact ltd")
-        user_input = input("Enter Selection")
+        user_input = input("Enter Selection\n")
         if(int(user_input) == 1):
-               
-        if(int(user_input) == 1):
-        if(int(user_input) == 1):
+            for k in self.contact_book.values():
+                print("Contact ID: {}\nContact Name: {}".format(k['contact_ID'],k['contact_name']))
+            input("....")
+        if(int(user_input) == 2):
+            for k in self.contact_book.values():
+                print("Contact ID: {}\nContact Name: {}".format(k['contact_ID'],k['contact_billing']))
+            input("....")
+        if(int(user_input) == 3):
+            for k in self.contact_book.values():
+                print("Contact ID: {}\nContact Name: {}".format(k['contact_ID'],k['contact_LTD']))
+            input("....")
+                   
+
     def save_contacts(self):
-        with open("contacts.txt","w") as file:
+        with open("contacts.txt", "w") as file:
             file.write(json.dumps(self.contact_book))
+
     def load_contacts(self):
         with open("contacts.txt", "r+") as file:
             self.contact_book = json.load(file)
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from invoice_manager import Invoice_manager
 from Contact_manager import Contact_manager
+
+
 inv_mgr = Invoice_manager()
 con_mgr = Contact_manager()
-#Menu
+# Menu
+
+
 def main_menu():
     print("options should be:")
     print("1:\tCreate contact")
@@ -10,24 +14,33 @@ def main_menu():
     print("3:\tSave contacts")
     print("4:\tload contacts")
     print("5:\tmodify contact")
-    input_option = input("Enter Option:")
-    print(input_option) 
+    input_option = input("Enter Option:\n")
+    
+    
     if(int(input_option) == 1):
         con_mgr.create_contact()
-        print(con_mgr.display_contact())
+        print(con_mgr.contact_book)
+        main_menu()
+        
     if(int(input_option) == 2):
-        print(con_mgr.display_contact())
+        print("launched 2")
+        con_mgr.display_contact()
+    
     if(int(input_option) == 3):
         con_mgr.save_contacts()
+    
     if(int(input_option) == 4):
         con_mgr.load_contacts()
         main_menu()
+        
     if(int(input_option) == 5):
         user_input = input("Enter Contact ID\n")
         con_mgr.modify_contact(user_input)
         main_menu()
+    
     else:
         print("invalid selection")
         main_menu()
+
 
 main_menu()
